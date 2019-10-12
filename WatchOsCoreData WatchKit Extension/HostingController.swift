ed5cb8a7a1456952fb7533fb8f9fe6e4739d8e6f
SchwiftyUI <schwiftyui@gmail.com>
2019-10-12 13:17:54 -0700

    Initial Commit

---
+++ WatchOsCoreData WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchOsCoreData WatchKit Extension
+//
+//  Created by SchwiftyUI on 10/12/19.
+//  Copyright © 2019 SchwiftyUI. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
